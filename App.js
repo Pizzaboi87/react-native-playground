994c988f21cc4c8c6a67419afb97ad43ffcb558b
Peter Weiser <peterweiser87@gmail.com>
2023-03-06 22:40:26 +0200

    Added image

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>Szia <Text style={styles.textBold}>Carlos Nunez</Text>!{"\n"}Hogy vagy?</Text>
       <StatusBar style="auto" />
+      <Image 
+        style={styles.image}
+        source={{
+          uri: 'https://avatarfiles.alphacoders.com/988/98825.jpg'
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.15,
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -27,5 +33,10 @@ const styles = StyleSheet.create({
   textBold: {
     fontWeight: 'bold',
     color: 'green'
+  },
+  image: {
+    marginTop: 50,
+    width: 200,
+    height: 200
   }
 });
