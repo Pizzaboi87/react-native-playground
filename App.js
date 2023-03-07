53793854c22d4d4319efc4903236a651e24ce20d
Peter Weiser <peterweiser87@gmail.com>
2023-03-07 23:35:31 +0200

    Added user input

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { View, StyleSheet } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MenuItems from './components/MenuItems';
+import WriteReview from './components/WriteReview';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <Header />
-        <MenuItems />
+        {/*<MenuItems />*/}
+        <WriteReview />
       </View>
       <View style={styles.footerContainer}>
         <Footer />
