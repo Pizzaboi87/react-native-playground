dbd04707183c968eea7ba145e27f15a69fc1ce4c
Peter Weiser <peterweiser87@gmail.com>
2023-03-09 23:13:02 +0200

    Added routes and welcome screen.

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MenuItems from './components/MenuItems';
 import WriteReview from './components/WriteReview';
+import WelcomeScreen from './components/WelcomeScreen';
+import LoginScreen from './components/LoginScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
     <>
-      <View style={styles.container}>
-        <Header />
-        {/*<MenuItems />*/}
-        <WriteReview />
-      </View>
-      <View style={styles.footerContainer}>
-        <Footer />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Header />
+          <Stack.Navigator
+            initialRouteName="Home"
+            style={styles.footerContainer}
+            screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>
+              <Stack.Screen name="Home" component={WelcomeScreen} />
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+              <Stack.Screen name="Menu" component={MenuItems} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <Footer />
+        </View>
+      </NavigationContainer>
     </>
   );
 }
